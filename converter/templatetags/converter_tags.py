1c2cadbfa1e17523e4f6a38254decd122838eec7
Vera Mazhuga <vero4ka.ru@gmail.com>
2015-09-05 12:18:41 +0200

    Add filter to parse task result

--- converter/templatetags/converter_tags.py
+++ converter/templatetags/converter_tags.py
@@ -1,10 +1,14 @@
+import re
+
 from django import template
 from django.utils.safestring import mark_safe
+from django.conf import settings
 
 from djcelery.admin import TASK_STATE_COLORS
 
 
 register = template.Library()
+task_file_pattern = re.compile('[\w\-]+\.jpg')
 
 
 @register.filter
@@ -12,3 +16,14 @@ def colored_state(task):
     color = TASK_STATE_COLORS.get(task.state, 'black')
     state = '<b><span style="color: {0};">{1}</span></b>'.format(color, task.state)
     return mark_safe(state)
+
+
+@register.filter
+def get_task_url(task):
+    result = task_file_pattern.search(task.result)
+    if result:
+        return '{0}{1}'.format(
+            settings.MEDIA_URL,
+            result.group()
+        )
+    return '#'
